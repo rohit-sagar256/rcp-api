81a8b5eef744cf0a82ddba18040c8391489f9533
rohit-sagar256 <dev.rohit256@gmail.com>
2025-09-23 22:33:06 +0530

    add tahs

--- src/recipe/tests/test_recipe.py
+++ src/recipe/tests/test_recipe.py
@@ -11,7 +11,11 @@ from rest_framework.test import APIClient
 
 from src.core.models import Recipe
 
-from src.recipe.serializers import RecipeSerializer
+from src.recipe.serializers import (
+  RecipeSerializer,
+  RecipeDetailSerializer
+
+  )
 
 User= get_user_model()
 
@@ -19,6 +23,9 @@ User= get_user_model()
 RECIPE_URL = reverse("recipe:recipe-list")
 
 
+def detail_url(reciept_id):
+  """ create and recipe detail url """
+  return reverse("recipe:recipe-detail", args=[reciept_id])
 
 @pytest.fixture
 def user_cl(db):
@@ -94,4 +101,129 @@ def test_recipe_list_limited_to_user(authenticated_user, user_cl):
 
 
 
+def test_get_recipe_detail(authenticated_user, user_cl):
+  """ Test get recipe detail """
+  recipe = create_recipe(user=user_cl)
 
+  url = detail_url(reciept_id=recipe.id)
+
+  response = authenticated_user.get(url)
+
+  serializer = RecipeDetailSerializer(recipe)
+
+  assert response.status_code == status.HTTP_200_OK
+
+  assert serializer.data == response.data
+
+def test_create_recipe(authenticated_user, user_cl):
+  """ Test creating a recipe """
+
+  payload = {
+    "title": "Sample Recipe",
+    "time_minutes": 30,
+    "price": Decimal("5.99"),
+    "description": "Sample description",
+    "link": "https://example.com/recipe.pdf"
+  }
+
+  response = authenticated_user.post(RECIPE_URL, payload)
+
+  assert response.status_code == status.HTTP_201_CREATED
+
+  recipe = Recipe.objects.get(id=response.data["id"])
+
+  for key in payload.keys():
+    assert payload[key] == getattr(recipe, key)
+
+  assert recipe.user == user_cl
+
+def test_partial_update_recipe(authenticated_user, user_cl):
+  original_link = "https://example.com/recipe.pdf"
+  updated_link = "https://example.com/new-recipe.pdf"
+  recipe = create_recipe(
+    user=user_cl,
+    title="Sample recipe title",
+    link=original_link,
+    description="Sample description"
+  )
+
+  response = authenticated_user.patch(
+    detail_url(recipe.id),
+    {"title": "New recipe title", "link": updated_link}
+  )
+
+  assert response.status_code == status.HTTP_200_OK
+  recipe.refresh_from_db()
+  assert recipe.title == "New recipe title"
+  assert recipe.link == updated_link
+  assert recipe.user == user_cl
+
+
+def test_full_update_recipe(authenticated_user, user_cl):
+  recipe = create_recipe(
+    user=user_cl,
+    title="Sample recipe title",
+    link="https://example.com/recipe.pdf",
+    description="Sample description"
+  )
+  payload = {
+    "title": "New recipe title",
+    "link": "https://example.com/new-recipe.pdf",
+    "description": "New sample description",
+    "time_minutes": 10,
+    "price": Decimal("2.50")
+  }
+  response = authenticated_user.put(
+    detail_url(recipe.id),
+    payload
+  )
+  assert response.status_code == status.HTTP_200_OK
+  recipe.refresh_from_db()
+  for key,value in payload.items():
+    assert value == getattr(recipe, key)
+
+
+def test_changing_user_on_update_recipe(authenticated_user, user_cl):
+  """ Test changing the recipe user when updating the recipe """
+  new_user = User.objects.create_user(
+    email="testnewuser@gmail.com",
+    password="testpass123",
+  )
+
+  recipe = create_recipe(user=user_cl)
+
+  response = authenticated_user.patch(
+    detail_url(recipe.id),
+    {"user": new_user.id}
+  )
+
+  assert response.status_code == status.HTTP_200_OK
+  recipe.refresh_from_db()
+  assert recipe.user == user_cl
+  assert recipe.user != new_user
+
+
+def test_delete_recipe(authenticated_user, user_cl):
+  """ Test deleting a recipe successful """
+  recipe = create_recipe(user=user_cl)
+
+  response = authenticated_user.delete(
+    detail_url(recipe.id)
+  )
+
+  assert response.status_code == status.HTTP_204_NO_CONTENT
+  assert not Recipe.objects.filter(id=recipe.id).exists()
+
+
+def test_delete_other_users_recipe_error(authenticated_user, user_cl):
+  """ Test trying to delete another user's recipe gives error """
+  new_user = User.objects.create_user(
+    email="newusertest@gmail.com",
+    password="testpass123",
+  )
+  recipe = create_recipe(user=new_user)
+  response = authenticated_user.delete(
+    detail_url(recipe.id)
+  )
+  assert response.status_code == status.HTTP_404_NOT_FOUND
+  assert Recipe.objects.filter(id=recipe.id).exists()
--- src/recipe/views.py
+++ src/recipe/views.py
@@ -7,7 +7,7 @@ from src.core.models import Recipe
 from src.recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
-  serializer_class = serializers.RecipeSerializer
+  serializer_class = serializers.RecipeDetailSerializer
   queryset = Recipe.objects.all()
   authentication_classes = [TokenAuthentication]
   permission_classes = [IsAuthenticated]
@@ -16,4 +16,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
   def get_queryset(self):
     """ Return reciepes for authenticated user """
     return self.queryset.filter(user=self.request.user).order_by("-id")
+
+
+  def get_serializer_class(self):
+
+    if self.action == "list":
+      return serializers.RecipeSerializer
+    return self.serializer_class
+
+  def perform_create(self, serializer):
+    """ Create a new recipe """
+    serializer.save(user=self.request.user)
+
+
 
